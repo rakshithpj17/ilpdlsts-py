d76027d9be593275f8982e42325a3d3a2407ab9d
rpj17-santec <rakshith.pj@santec.com>
2025-07-16 18:05:37 +0900

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# IL PDL STS - Python
+
+**Python Package for Santec Insertion Loss and Polarization Dependent Loss Swept Test System.**
---
+++ ilpdlsts/__init__.py
@@ -0,0 +1,4 @@
+# ilpdlsts/__init__.py
+"""
+ilpdlsts - Python Package for Santec Insertion Loss and Polarization Dependent Loss Swept Test System.
+"""
